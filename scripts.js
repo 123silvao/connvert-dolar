06ae52c25e877ac5cb0f6a161141c3034355cfb6
123silvao <sc.silvio321@gmail.com>
2025-05-04 09:00:05 -0300

    arquivo enviado

--- scripts.js
+++ scripts.js
@@ -1,35 +1,20 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
+// const dolarToday = 5.670
+// const euroToday = 6.20
+// const bitcoinToday = 6.20
+// const libraToday = 500.882
 
-
-function convertValues() {
+const convertValues = async () => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
     const currencyValueConverted = document.querySelector(".currency-value")// valor em dolar
 
-
-
-    const dolarToday = 5.670
-    const euroToday = 6.20
-    const bitcoinToday = 6.20
-    const libraToday = 500.882
-
-
-    if (currencySelect.value == "dolar") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-        }).format(inputCurrencyValue * dolarToday)
-    }
-
-    if (currencySelect.value == "euro") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-            style: "currency",
-            currency: "EUR"
-        }).format(inputCurrencyValue * euroToday)
-    }
-
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(Response => Response.json())
+    const dolar = data.USDBRL.hight
+    const euro = data.USDBRL.hight
+    
 
 
 
@@ -38,9 +23,25 @@ function convertValues() {
         currency: "BRL"
     }).format(inputCurrencyValue)
 
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolar);
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue /euro)
+    }
+
+
 }
 
-function changeCurrency(){
+function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
     const currencyImage = document.querySelector(".currency-img")
 
